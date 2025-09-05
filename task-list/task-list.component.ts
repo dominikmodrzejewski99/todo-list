a26457a420635382a6591f63fe09344bbe6d951e
Dominik Modrzejewski <dominik.modrzejewski@wfirma.pl>
2025-09-05 12:19:23 +0200

    feat: enhance the task list UI with new styles and structure

--- task-list/task-list.component.ts
+++ task-list/task-list.component.ts
@@ -1,26 +1,38 @@
-import {Component, inject} from '@angular/core';
-import {TasksListService} from '../services/tasks-list.service';
-import {FormsModule} from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { TasksListService } from '../services/tasks-list.service';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-task-list',
   imports: [
-    FormsModule
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule
   ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
-    tasksListService: TasksListService = inject(TasksListService);
+  tasksListService: TasksListService = inject(TasksListService);
 
 
-    newTaskText = this.tasksListService.newTaskText;
-    filteredTodos = this.tasksListService.filteredTodos;
-    addTask() {
-      this.tasksListService.addTask(this.newTaskText());
+  newTaskText = this.tasksListService.newTaskText;
+  filteredTodos = this.tasksListService.filteredTodos;
+  addTask() {
+    this.tasksListService.addTask(this.newTaskText());
 
-      this.newTaskText.set('');
+    this.newTaskText.set('');
 
 
-    }
+  }
 }
